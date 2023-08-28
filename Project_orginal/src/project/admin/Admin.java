bd5c75fbd6b769aaf0083051d15e1968839df260
USER <USER@DESKTOP-R8G7MVN>
2023-08-28 14:39:35 +0900

    upload

--- Project_orginal/src/project/admin/Admin.java
+++ Project_orginal/src/project/admin/Admin.java
@@ -2,5 +2,46 @@ package project.admin;
 
 public class Admin {
     private int adminId;
+    private int playerId;
+    private int blackListId;
+    
+    public Admin() {}
+
+	public Admin(int adminId, int playerId, int blackListId) {
+		super();
+		this.adminId = adminId;
+		this.playerId = playerId;
+		this.blackListId = blackListId;
+	}
+
+	public int getAdminId() {
+		return adminId;
+	}
+
+	public void setAdminId(int adminId) {
+		this.adminId = adminId;
+	}
+
+	public int getPlayerId() {
+		return playerId;
+	}
+
+	public void setPlayerId(int playerId) {
+		this.playerId = playerId;
+	}
+
+	public int getBlackListId() {
+		return blackListId;
+	}
+
+	public void setBlackListId(int blackListId) {
+		this.blackListId = blackListId;
+	}
+
+	@Override
+	public String toString() {
+		return "Admin [adminId=" + adminId + ", playerId=" + playerId + ", blackListId=" + blackListId + "]";
+	}
+    
 
 }
--- Project_orginal/src/project/admin/AdminDao.java
+++ Project_orginal/src/project/admin/AdminDao.java
@@ -1,4 +1,13 @@
 package project.admin;
 
+import project.config.DBConnect;
+
 public class AdminDao {
+	private DBConnect dbconn;
+	
+	public AdminDao() {
+		dbconn=DBConnect.getInstance();
+	}
+	
+	
 }
