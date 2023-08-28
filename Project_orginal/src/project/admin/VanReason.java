7a24c9e952d7b7a1499fd5a80ebf13910f9f94de
USER <USER@DESKTOP-R8G7MVN>
2023-08-28 17:23:08 +0900

    ban 추가

--- Project_orginal/src/project/admin/VanReason.java
+++ Project_orginal/src/project/admin/VanReason.java
@@ -1,5 +1,19 @@
 package project.admin;
 
 public enum VanReason {
-    욕설, 게임방해, 버그악용
+    욕설(1), 게임방해(2), 버그악용(3);
+    int Ban;
+
+	private VanReason(int ban) {
+		Ban = ban;
+	}
+
+	public int getBan() {
+		return Ban;
+	}
+
+	public void setBan(int ban) {
+		Ban = ban;
+	}
+
 }
